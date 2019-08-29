194e1c4ab884bac4c9f1209c08930cf32973687a
Azzy <azzy049@gmail.com>
2019-08-30 03:04:11 +0530

    From home 30/08/19

--- app.py
+++ app.py
@@ -69,28 +69,64 @@ Tracker : https://trello.com/b/gVSNGEOf/trace-parser
 def index():
     return render_template('index.html')
 
-@app.route("/register", methods=['GET', 'POST'])
-def register():
-    if request.method == 'POST' and 'trace_file' in request.files:
-        f = request.files['trace_file']
-        fname = os.path.splitext(f.filename)
+
+@app.route("/show_trace_logs", methods=['GET', 'POST'])
+def show_trace_logs():
+    try:
+        file = open('sample.json','r')
+        data = json.load(file)
+        file.close()
+    except FileNotFoundError:
+        print("the file not found, exiting...")
+        data= []
+    return render_template('traceview.html', data=data)
+
+@app.route("/show_snoop_logs", methods=['GET', 'POST'])
+def show_snoop_logs():
+    if request.method == 'POST':
+        f = request.files['snoop_file']
+        fname = f.filename
         dirname = os.getcwd()
         print(dirname)
-        f.save(secure_filename(f.filename))
-        return render_template("index.html", )
+        if fname == '':
+            flash("Are you sure that's correct file?",error_class)
+            return render_template("index.html")
+        else:
+            try:
+                f.save(secure_filename(f.filename))
+            except expression as identifier:
+                flash("Are you sure that's correct file?",error_class)
+                return render_template("index.html")
+            finally:
+                flash("File Saved",success_class)
+                return render_template("index.html")
+    else:
+        flash("Somethin Wrong, try again",error_class)
+        return render_template("index.html")
 
 
-    '''  
-    result = execution()
-    if result:
-        try:
-            file = open('sample.json','r')
-            data = json.load(file)
-            file.close()
-        except FileNotFoundError:
-            print("the file not found, exiting...")
-            data= []
-    '''
+@app.route("/trace_only", methods=['GET', 'POST'])
+def trace_only():
+    if request.method == 'POST':
+        f = request.files['trace_file']
+        fname = f.filename
+        dirname = os.getcwd()
+        print(dirname)
+        if fname == '':
+            flash("Are you sure that's correct file?",error_class)
+            return render_template("index.html")
+        else:
+            try:
+                f.save(secure_filename(f.filename))
+            except expression as identifier:
+                flash("Are you sure that's correct file?",error_class)
+                return render_template("index.html")
+            finally:
+                flash("File Saved",success_class)
+                return render_template("index.html")
+    else:
+        flash("Somethin Wrong, try again",error_class)
+        return render_template("index.html")
 
 
 if __name__ == '__main__':
